8f95523b79b4cc4d6dfa7da1678ab89cb0d1d390
fishdoctor1 <kantasak.singyabut@gmail.com>
2017-01-21 00:25:33 +0700

    CropImage Can not save

---
+++ CropImage/CropImage/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace CropImage
+{
+    partial class CropImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SrcPicBox = new System.Windows.Forms.PictureBox();
+            this.Source_Picture = new System.Windows.Forms.Label();
+            this.lbCordinates = new System.Windows.Forms.Label();
+            this.Crop = new System.Windows.Forms.Button();
+            this.TargetPicBox = new System.Windows.Forms.PictureBox();
+            this.chkCropCordinates = new System.Windows.Forms.CheckBox();
+            this.tbCordinates = new System.Windows.Forms.TextBox();
+            this.Select_Image = new System.Windows.Forms.Button();
+            this.Target_Picture = new System.Windows.Forms.Label();
+            this.Save_Image = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Filename = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SrcPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TargetPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SrcPicBox
+            // 
+            this.SrcPicBox.Location = new System.Drawing.Point(15, 25);
+            this.SrcPicBox.Name = "SrcPicBox";
+            this.SrcPicBox.Size = new System.Drawing.Size(710, 730);
+            this.SrcPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.SrcPicBox.TabIndex = 0;
+            this.SrcPicBox.TabStop = false;
+            this.SrcPicBox.Paint += new System.Windows.Forms.PaintEventHandler(this.SrcPicBox_Paint);
+            this.SrcPicBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SrcPicBox_MouseDown);
+            this.SrcPicBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SrcPicBox_MouseMove);
+            this.SrcPicBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SrcPicBox_MouseUp);
+            // 
+            // Source_Picture
+            // 
+            this.Source_Picture.AutoSize = true;
+            this.Source_Picture.Location = new System.Drawing.Point(12, 9);
+            this.Source_Picture.Name = "Source_Picture";
+            this.Source_Picture.Size = new System.Drawing.Size(80, 13);
+            this.Source_Picture.TabIndex = 1;
+            this.Source_Picture.Text = "Source_Picture";
+            // 
+            // lbCordinates
+            // 
+            this.lbCordinates.AutoSize = true;
+            this.lbCordinates.Location = new System.Drawing.Point(511, 9);
+            this.lbCordinates.Name = "lbCordinates";
+            this.lbCordinates.Size = new System.Drawing.Size(67, 13);
+            this.lbCordinates.TabIndex = 2;
+            this.lbCordinates.Text = "Coordinate : ";
+            // 
+            // Crop
+            // 
+            this.Crop.Location = new System.Drawing.Point(731, 60);
+            this.Crop.Name = "Crop";
+            this.Crop.Size = new System.Drawing.Size(119, 29);
+            this.Crop.TabIndex = 3;
+            this.Crop.Text = "Crop";
+            this.Crop.UseVisualStyleBackColor = true;
+            this.Crop.Click += new System.EventHandler(this.Crop_Click);
+            // 
+            // TargetPicBox
+            // 
+            this.TargetPicBox.Location = new System.Drawing.Point(856, 25);
+            this.TargetPicBox.Name = "TargetPicBox";
+            this.TargetPicBox.Size = new System.Drawing.Size(355, 365);
+            this.TargetPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.TargetPicBox.TabIndex = 4;
+            this.TargetPicBox.TabStop = false;
+            // 
+            // chkCropCordinates
+            // 
+            this.chkCropCordinates.AutoSize = true;
+            this.chkCropCordinates.Location = new System.Drawing.Point(98, 8);
+            this.chkCropCordinates.Name = "chkCropCordinates";
+            this.chkCropCordinates.Size = new System.Drawing.Size(202, 17);
+            this.chkCropCordinates.TabIndex = 5;
+            this.chkCropCordinates.Text = "Crop using coordinates ( x1,y1,x2,y2 )";
+            this.chkCropCordinates.UseVisualStyleBackColor = true;
+            this.chkCropCordinates.CheckedChanged += new System.EventHandler(this.chkCropCordinates_CheckedChanged);
+            // 
+            // tbCordinates
+            // 
+            this.tbCordinates.Location = new System.Drawing.Point(306, 3);
+            this.tbCordinates.Name = "tbCordinates";
+            this.tbCordinates.Size = new System.Drawing.Size(115, 20);
+            this.tbCordinates.TabIndex = 6;
+            this.tbCordinates.Visible = false;
+            this.tbCordinates.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCordinates_KeyPress);
+            // 
+            // Select_Image
+            // 
+            this.Select_Image.Location = new System.Drawing.Point(731, 25);
+            this.Select_Image.Name = "Select_Image";
+            this.Select_Image.Size = new System.Drawing.Size(119, 29);
+            this.Select_Image.TabIndex = 7;
+            this.Select_Image.Text = "Select Image";
+            this.Select_Image.UseVisualStyleBackColor = true;
+            this.Select_Image.Click += new System.EventHandler(this.Select_Image_Click);
+            // 
+            // Target_Picture
+            // 
+            this.Target_Picture.AutoSize = true;
+            this.Target_Picture.Location = new System.Drawing.Point(853, 9);
+            this.Target_Picture.Name = "Target_Picture";
+            this.Target_Picture.Size = new System.Drawing.Size(77, 13);
+            this.Target_Picture.TabIndex = 8;
+            this.Target_Picture.Text = "Target_Picture";
+            // 
+            // Save_Image
+            // 
+            this.Save_Image.Location = new System.Drawing.Point(856, 445);
+            this.Save_Image.Name = "Save_Image";
+            this.Save_Image.Size = new System.Drawing.Size(118, 32);
+            this.Save_Image.TabIndex = 9;
+            this.Save_Image.Text = "Save_Image";
+            this.Save_Image.UseVisualStyleBackColor = true;
+            this.Save_Image.Click += new System.EventHandler(this.Save_Image_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(856, 419);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Filename
+            // 
+            this.Filename.AutoSize = true;
+            this.Filename.Location = new System.Drawing.Point(853, 403);
+            this.Filename.Name = "Filename";
+            this.Filename.Size = new System.Drawing.Size(49, 13);
+            this.Filename.TabIndex = 11;
+            this.Filename.Text = "Filename";
+            // 
+            // CropImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1284, 651);
+            this.Controls.Add(this.Filename);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Save_Image);
+            this.Controls.Add(this.Target_Picture);
+            this.Controls.Add(this.Select_Image);
+            this.Controls.Add(this.tbCordinates);
+            this.Controls.Add(this.chkCropCordinates);
+            this.Controls.Add(this.TargetPicBox);
+            this.Controls.Add(this.Crop);
+            this.Controls.Add(this.lbCordinates);
+            this.Controls.Add(this.Source_Picture);
+            this.Controls.Add(this.SrcPicBox);
+            this.Name = "CropImage";
+            this.Text = "CropImage";
+            ((System.ComponentModel.ISupportInitialize)(this.SrcPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TargetPicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SrcPicBox;
+        private System.Windows.Forms.Label Source_Picture;
+        private System.Windows.Forms.Label lbCordinates;
+        private System.Windows.Forms.Button Crop;
+        private System.Windows.Forms.PictureBox TargetPicBox;
+        private System.Windows.Forms.CheckBox chkCropCordinates;
+        private System.Windows.Forms.TextBox tbCordinates;
+        private System.Windows.Forms.Button Select_Image;
+        private System.Windows.Forms.Label Target_Picture;
+        private System.Windows.Forms.Button Save_Image;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Filename;
+    }
+}
+
---
+++ CropImage/CropImage/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace CropImage
+{
+    public partial class CropImage : Form
+    {
+        Boolean bHaveMouse;
+        Point ptOriginal = new Point();
+        Point ptLast = new Point();
+        Rectangle rectCropArea;
+        //Image srcImage = null;
+        //Graphics g;
+        //Bitmap sourceBitmap { get; set; }
+
+        public CropImage()
+        {
+            bHaveMouse = false;
+            InitializeComponent();
+            
+        }        
+
+        private void SrcPicBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Set internal flag to know we no longer "have the mouse".
+            bHaveMouse = false;
+
+            // If we have drawn previously, draw again in that spot
+            // to remove the lines.
+            if (ptLast.X != -1)
+            {
+                Point ptCurrent = new Point(e.X, e.Y);
+
+                // Display coordinates
+                lbCordinates.Text = "Coordinates  :  " + ptOriginal.X.ToString() + ", " +
+                    ptOriginal.Y.ToString() + " And " + e.X.ToString() + ", " + e.Y.ToString();
+
+            }
+
+            // Set flags to know that there is no "previous" line to reverse.
+            ptLast.X = -1;
+            ptLast.Y = -1;
+            ptOriginal.X = -1;
+            ptOriginal.Y = -1;
+        }
+
+        private void SrcPicBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Make a note that we "have the mouse".
+            bHaveMouse = true;
+
+            // Store the "starting point" for this rubber-band rectangle.
+            ptOriginal.X = e.X;
+            ptOriginal.Y = e.Y;
+
+            // Special value lets us know that no previous
+            // rectangle needs to be erased.
+
+            // Display coordinates
+            lbCordinates.Text = "Coordinates  :  " + e.X.ToString() + ", " + e.Y.ToString();
+
+            ptLast.X = -1;
+            ptLast.Y = -1;
+
+            rectCropArea = new Rectangle(new Point(e.X, e.Y), new Size());
+        }
+
+        private void SrcPicBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point ptCurrent = new Point(e.X, e.Y);
+
+            // If we "have the mouse", then we draw our lines.
+            if (bHaveMouse)
+            {
+                // If we have drawn previously, draw again in
+                // that spot to remove the lines.
+                if (ptLast.X != -1)
+                {
+                    // Display Coordinates
+                    lbCordinates.Text = "Coordinates  :  " + ptOriginal.X.ToString() + ", " +
+                        ptOriginal.Y.ToString() + " And " + e.X.ToString() + ", " + e.Y.ToString();
+                }
+
+                // Update last point.
+                ptLast = ptCurrent;
+
+                // Draw new lines.
+
+                // e.X - rectCropArea.X;
+                // normal
+                if (e.X > ptOriginal.X && e.Y > ptOriginal.Y)
+                {
+                    rectCropArea.Width = e.X - ptOriginal.X;
+
+                    // e.Y - rectCropArea.Height;
+                    rectCropArea.Height = e.Y - ptOriginal.Y;
+                }
+                else if (e.X < ptOriginal.X && e.Y > ptOriginal.Y)
+                {
+                    rectCropArea.Width = ptOriginal.X - e.X;
+                    rectCropArea.Height = e.Y - ptOriginal.Y;
+                    rectCropArea.X = e.X;
+                    rectCropArea.Y = ptOriginal.Y;
+                }
+                else if (e.X > ptOriginal.X && e.Y < ptOriginal.Y)
+                {
+                    rectCropArea.Width = e.X - ptOriginal.X;
+                    rectCropArea.Height = ptOriginal.Y - e.Y;
+
+                    rectCropArea.X = ptOriginal.X;
+                    rectCropArea.Y = e.Y;
+                }
+                else
+                {
+                    rectCropArea.Width = ptOriginal.X - e.X;
+
+                    // e.Y - rectCropArea.Height;
+                    rectCropArea.Height = ptOriginal.Y - e.Y;
+                    rectCropArea.X = e.X;
+                    rectCropArea.Y = e.Y;
+                }
+                SrcPicBox.Refresh();
+            }
+        }
+       
+        private void Crop_Click(object sender, EventArgs e)
+        {
+            TargetPicBox.Refresh();
+
+            //Prepare a new Bitmap on which the cropped image will be drawn
+            Bitmap sourceBitmap = new Bitmap(SrcPicBox.Image, SrcPicBox.Width, SrcPicBox.Height);
+            Graphics g = TargetPicBox.CreateGraphics();
+
+            //Draw the image on the Graphics object with the new dimesions
+                                        
+                g.DrawImage(sourceBitmap, new Rectangle(0, 0, TargetPicBox.Width, TargetPicBox.Height),
+                rectCropArea, GraphicsUnit.Pixel);
+                g = Graphics.FromImage(sourceBitmap);
+
+            //Good practice to dispose the System.Drawing objects when not in use.
+            //sourceBitmap.Dispose();
+            //Bitmap targetBitmap = new Bitmap(TargetPicBox.Image, TargetPicBox.Width, TargetPicBox.Height);
+            sourceBitmap.Save(AppDomain.CurrentDomain.BaseDirectory + "castle_icon.jpg", ImageFormat.Jpeg);
+        }
+
+        private void chkCropCordinates_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkCropCordinates.Checked)
+            {
+                tbCordinates.Visible = true;
+            }
+            else
+            {
+                tbCordinates.Visible = false;
+            }
+        }
+
+        private void SrcPicBox_Paint(object sender, PaintEventArgs e)
+        {
+            Pen drawLine = new Pen(Color.Black);
+            drawLine.DashStyle = DashStyle.Dash;
+            e.Graphics.DrawRectangle(drawLine, rectCropArea);
+        }
+
+        private void Select_Image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Image img = Image.FromFile(open.FileName);
+                SrcPicBox.Image = img;
+            }
+        }
+
+        private void tbCordinates_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Following allows only numbers and comma for givng expected input
+            if (!char.IsControl(e.KeyChar)
+       && !char.IsDigit(e.KeyChar)
+       && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+
+            //Only allow comma as separator for specifying co-ordinates
+            if (e.KeyChar == ',')
+            {
+                e.Handled = false;
+            }
+
+        }
+
+        private void Save_Image_Click(object sender, EventArgs e)
+        {
+            //// Displays a SaveFileDialog so the user can save the Image
+            //// assigned to Button2.
+            //SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            //saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            //saveFileDialog1.Title = "Save an Image File";
+            //saveFileDialog1.ShowDialog();
+
+            //// If the file name is not an empty string open it for saving.
+            //if (saveFileDialog1.FileName != "")
+            //{
+            //    // Saves the Image via a FileStream created by the OpenFile method.
+            //    System.IO.FileStream fs =
+            //       (System.IO.FileStream)saveFileDialog1.OpenFile();
+            //    // Saves the Image in the appropriate ImageFormat based upon the
+            //    // File type selected in the dialog box.
+            //    // NOTE that the FilterIndex property is one-based.
+            //    switch (saveFileDialog1.FilterIndex)
+            //    {
+            //        case 1:
+            //            this.Save_Image.Image.Save(fs,
+            //               System.Drawing.Imaging.ImageFormat.Jpeg);
+            //            break;
+
+            //        case 2:
+            //            this.Save_Image.Image.Save(fs,
+            //               System.Drawing.Imaging.ImageFormat.Bmp);
+            //            break;
+
+            //        case 3:
+            //            this.Save_Image.Image.Save(fs,
+            //               System.Drawing.Imaging.ImageFormat.Gif);
+            //            break;
+            //    }
+
+            //    fs.Close();
+            //}           
+                TargetPicBox.Image.Save(@"E:\100000test.jpg", ImageFormat.Jpeg);
+            //Invalidate(true);
+        }
+    }
+}
