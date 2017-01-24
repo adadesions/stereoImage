2af5b91d21b8c42f418de98a1afa0aee95a67b90
fishdoctor1 <kantasak.singyabut@gmail.com>
2017-01-24 11:32:57 +0700

    Change emgu2 to TrainPointFace

---
+++ TrainPointFace/TrainPointFace/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TrainPointFace
+{
+    partial class TrainPointFace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.save_txt = new System.Windows.Forms.Button();
+            this.LoadPoint = new System.Windows.Forms.Button();
+            this.Select_Image = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(710, 730);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(767, 25);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(730, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Face";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(733, 61);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(130, 45);
+            this.save.TabIndex = 3;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // save_txt
+            // 
+            this.save_txt.Location = new System.Drawing.Point(732, 112);
+            this.save_txt.Name = "save_txt";
+            this.save_txt.Size = new System.Drawing.Size(130, 45);
+            this.save_txt.TabIndex = 4;
+            this.save_txt.Text = "save txt";
+            this.save_txt.UseVisualStyleBackColor = true;
+            this.save_txt.Click += new System.EventHandler(this.save_txt_Click);
+            // 
+            // LoadPoint
+            // 
+            this.LoadPoint.Location = new System.Drawing.Point(732, 163);
+            this.LoadPoint.Name = "LoadPoint";
+            this.LoadPoint.Size = new System.Drawing.Size(130, 45);
+            this.LoadPoint.TabIndex = 5;
+            this.LoadPoint.Text = "LoadPoint";
+            this.LoadPoint.UseVisualStyleBackColor = true;
+            this.LoadPoint.Click += new System.EventHandler(this.LoadPoint_Click);
+            // 
+            // Select_Image
+            // 
+            this.Select_Image.Location = new System.Drawing.Point(732, 224);
+            this.Select_Image.Name = "Select_Image";
+            this.Select_Image.Size = new System.Drawing.Size(130, 45);
+            this.Select_Image.TabIndex = 6;
+            this.Select_Image.Text = "Select Image";
+            this.Select_Image.UseVisualStyleBackColor = true;
+            this.Select_Image.Click += new System.EventHandler(this.Select_Image_Click);
+            // 
+            // TrainPointFace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(942, 616);
+            this.Controls.Add(this.Select_Image);
+            this.Controls.Add(this.LoadPoint);
+            this.Controls.Add(this.save_txt);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "TrainPointFace";
+            this.Text = "TrainPointFace";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button save_txt;
+        private System.Windows.Forms.Button LoadPoint;
+        private System.Windows.Forms.Button Select_Image;
+    }
+}
+
---
+++ TrainPointFace/TrainPointFace/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TrainPointFace
+{
+    public partial class TrainPointFace : Form
+    {
+        public class InitialPoint
+        {
+            public int[] PointX { get; set; }
+            public int[] PointY { get; set; }
+            public int[] PointZ { get; set; }
+        }
+
+        public class PointOnFace
+        {
+            public int Face { get; set; }
+            public int[] PointX { get; set; }
+            public int[] PointY { get; set; }
+            public int[] PointZ { get; set; }
+        }
+        static string json = File.ReadAllText("../initialpoint.json");
+        static InitialPoint init = JsonConvert.DeserializeObject<InitialPoint>(json);
+        //Console.WriteLine(init.PointX[0]);
+
+        public static int[] pointx = init.PointX;
+        public static int[] pointy = init.PointY;
+
+        public int pointmove; //ตัวแปรกำหนดค่าว่าจุดไหนจะขยับตามเม้า
+        private Point MouseDownLocation;
+        Rectangle[] recStore = new Rectangle[37];
+
+        public TrainPointFace()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 37; i++)
+            {
+                recStore[i] = new Rectangle((pointx[i]), (pointy[i]), 10, 10);
+                e.Graphics.FillEllipse(Brushes.Red, recStore[i]);
+            }
+            Update(); //ลื่น ใช้กับจุดขยับตามเม้า
+            //Refresh(); //ตรงนี้ทำให้หน่วงและทำให้จุดขยับตามเม้าได้
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int[] differencex = new int[37];
+            int[] differencey = new int[37];
+            int holdx = 8, holdy = 8;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                for (int i = 0; i <= 36; i++) //calculate difference X,Y all point
+                {
+                    differencex[i] = Math.Abs(e.X - pointx[i]);
+                    differencey[i] = Math.Abs(e.Y - pointy[i]);
+                }
+
+                for (int i = 0; i <= 36; i++) //loop search point near mouse
+                {
+                    if (differencex[i] < holdx && differencey[i] < holdy)
+                    {
+                        holdx = differencex[i];
+                        holdy = differencey[i];
+                        pointmove = i;
+                        Console.WriteLine("Point" + pointmove);
+                        //finish search pointmove
+                    }
+                    //finish condition search pointmove
+                }
+                //end loop search pointmove
+                Invalidate();
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //pointmove
+                MouseDownLocation = e.Location;
+                recStore[pointmove] = new Rectangle(e.X, e.Y, 10, 10);
+                pointx[pointmove] = e.X;
+                pointy[pointmove] = e.Y;
+                Console.WriteLine("Point" + pointmove + "X = " + MouseDownLocation.X);
+                Console.WriteLine("Point" + pointmove + "Y = " + MouseDownLocation.Y);
+                Invalidate(true);
+                //finish pointmove
+            }
+        }
+
+        private void Select_Image_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Image img = Image.FromFile(open.FileName);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void LoadPoint_Click(object sender, EventArgs e)
+        {
+            decimal value = (numericUpDown1.Value); //face no
+            int faceInt = decimal.ToInt32(value); //convert to int            
+            bool checkfile = File.Exists("../PointFace" + faceInt + ".json");
+            Console.WriteLine(checkfile);
+            if (checkfile == true)
+            {
+                string json = File.ReadAllText("../PointFace" + faceInt + ".json");
+                InitialPoint init = JsonConvert.DeserializeObject<InitialPoint>(json);
+                for (int t = 0; t < 37; t++)
+                {
+                    pointx[t] = init.PointX[t];
+                    pointy[t] = init.PointY[t];
+                }
+                MessageBox.Show("Load Point Face" + faceInt);
+            }
+            else
+            {
+                MessageBox.Show("File Not Found");
+            }
+            Invalidate(true);
+        }
+
+        private void save_txt_Click(object sender, EventArgs e)
+        {
+            decimal value = (numericUpDown1.Value); //face no
+            int faceInt = decimal.ToInt32(value); //convert to int
+            DialogResult result = MessageBox.Show("Save To " + value, "Save txt", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                //yes...                
+                string face = faceInt.ToString();
+                PointOnFace updatepoint = new PointOnFace();
+                updatepoint.Face = faceInt;
+                updatepoint.PointX = pointx;
+                updatepoint.PointY = pointy;
+                //updatepoint.PointZ = pointz;
+                //string path = @"C:\emgu2\face" + face + ".txt";
+                string path = "../PointFace" + faceInt + ".txt";
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        for (int k = 0; k <= 36; k++)
+                        {
+                            sw.Write(pointx[k] + " " + pointy[k] + "\n");
+                        }
+                        sw.Close();
+                    }
+                    Console.WriteLine("File create!!!!");
+                }
+                MessageBox.Show("Save To Face" + faceInt);
+                Invalidate(true);
+
+            }
+            else if (result == DialogResult.No)
+            {
+                //no...
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            decimal value = (numericUpDown1.Value); //face no
+            int faceInt = decimal.ToInt32(value); //convert to int
+            DialogResult result = MessageBox.Show("Save To Face " + value, "Save Json", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                //yes...               
+                string face = faceInt.ToString();
+                PointOnFace updatepoint = new PointOnFace();
+                updatepoint.Face = faceInt;
+                updatepoint.PointX = pointx;
+                updatepoint.PointY = pointy;
+                //updatepoint.PointZ = pointz;
+
+                string json = JsonConvert.SerializeObject(updatepoint);
+
+                //write string to file
+                System.IO.File.WriteAllText("../PointFace" + faceInt + ".json", json);
+                //save to emgu2\bin\
+                Console.WriteLine("file create!!!!");
+                MessageBox.Show("Save To Face" + faceInt);
+            }
+            else if (result == DialogResult.No)
+            {
+                //no...
+            }
+        }
+    }
+}
